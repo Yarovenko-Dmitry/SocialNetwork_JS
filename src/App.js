09a9e803aa7e6722046c158b4e82191db522ebed
prostiak2019 <yarovenkodmv@gmail.com>
2020-05-10 19:42:59 +0300

    SN_v-28-JS-Move_data_into_index.js

--- src/App.js
+++ src/App.js
@@ -5,23 +5,23 @@ import Navbar from "./components/Navbar/Navbar";
 import Header from "./components/Header/Header";
 import Profile from "./components/Profile/Profile";
 import Dialogs from "./components/Dialogs/Dialogs";
-// import Route from "react-router-dom/es/Route";
-// import {BrowserRouter} from "react-router-dom";
 import {BrowserRouter, Route} from "react-router-dom";
 import News from "./components/News/News";
 import Music from "./components/Music/Music";
 import Settings from "./components/Settings/Settings";
 
-const App = () => {
+const App = (props) => {
+
   return (
     <BrowserRouter>
       <div className={'app-wrapper'}>
         <Header/>
         <Navbar/>
         <div className={'app-wrapper-content'}>
-          <Route path={'/dialogs'} component={Dialogs}/>
-          {/*<Route exact path={'/dialogs'} component={Dialogs}/>*/}
-          <Route path={'/profile'} component={Profile}/>
+          <Route path={'/profile'} render={() => <Profile posts={props.posts}/>}/>
+          <Route path={'/dialogs'} render={() => <Dialogs dialogs={props.dialogs} messages={props.messages}/>}/>
+
+
           <Route path={'/news'} component={News}/>
           <Route path={'/music'} component={Music}/>
           <Route path={'/settings'} component={Settings}/>
--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -1,48 +1,16 @@
 import React from "react";
 import s from './Dialogs.module.css'
-import {NavLink} from "react-router-dom";
+import {DialogsItem} from "./DialogItem/DialogItem";
+import {Message} from "./Message/Message";
 
-const DialogsItem = (props) => {
-  let path = '/dialogs/' + props.id
-  return (
-    <div className={s.dialog + ' ' + s.active}>
-      <NavLink to={path}>{props.name}</NavLink>
-    </div>
-  )
-};
+const Dialogs = (props) => {
 
-const Message = (props) => {
-  return (
-    <div className={s.message}>{props.message}</div>
-  )
-};
-
-const Dialogs = () => {
-
-  let dialogs = [
-    {id: 1, name: 'Dmitry'},
-    {id: 2, name: 'Andrey'},
-    {id: 3, name: 'Alexander'},
-    {id: 4, name: 'Victoriya'},
-    {id: 5, name: 'Nadezhda'},
-    {id: 6, name: 'Alex'},
-  ];
-
-  let messages = [
-    {id: 1, message: 'Hi'},
-    {id: 2, message: 'What it is'},
-    {id: 3, message: 'Go-go-go'},
-    {id: 4, message: 'Good'},
-    {id: 5, message: 'thanksgiving'},
-    {id: 6, message: 'fame and honor'},
-  ];
-
-  let dialogElements = dialogs
+  let dialogElements = props.dialogs
     .map(d =>
       <DialogsItem name={d.name} id={d.id}/>
     );
 
-  let messagesElements = messages
+  let messagesElements = props.messages
     .map(m =>
       <Message message={m.message}/>
     );
