aaf4e854e6d8b3e391a72c2277ebe8f90bbd45b3
prostiak2019 <yarovenkodmv@gmail.com>
2020-05-18 15:39:23 +0300

    SN_v-31-JS-onClick_ref_VirtualDOM

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import Navbar from "./components/Navbar/Navbar";
 import Header from "./components/Header/Header";
 import Profile from "./components/Profile/Profile";
 import Dialogs from "./components/Dialogs/Dialogs";
-import {BrowserRouter, Route} from "react-router-dom";
+import {Route} from "react-router-dom";
 import News from "./components/News/News";
 import Music from "./components/Music/Music";
 import Settings from "./components/Settings/Settings";
@@ -13,26 +13,24 @@ import Settings from "./components/Settings/Settings";
 const App = (props) => {
 
   return (
-    <BrowserRouter>
-      <div className={'app-wrapper'}>
-        <Header/>
-        <Navbar/>
-        <div className={'app-wrapper-content'}>
-          <Route path={'/profile'}
-                 render={() => <Profile state={props.state.profilePage}/>}/>
-          <Route path={'/dialogs'}
-                 render={() => <Dialogs state={props.state.dialogsPage}/>}/>
+    <div className={'app-wrapper'}>
+      <Header/>
+      <Navbar/>
+      <div className={'app-wrapper-content'}>
+        <Route path={'/profile'}
+               render={() => <Profile state={props.state.profilePage}/>}/>
+        <Route path={'/dialogs'}
+               render={() => <Dialogs state={props.state.dialogsPage}/>}/>
 
 
-          <Route path={'/news'}
-                 component={News}/>
-          <Route path={'/music'}
-                 component={Music}/>
-          <Route path={'/settings'}
-                 component={Settings}/>
-        </div>
+        <Route path={'/news'}
+               component={News}/>
+        <Route path={'/music'}
+               component={Music}/>
+        <Route path={'/settings'}
+               component={Settings}/>
       </div>
-    </BrowserRouter>
+    </div>
   );
 }
 
